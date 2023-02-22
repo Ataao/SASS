ce5b994a0df4595d81d79d550b03c4c7fd06c476
Bozo <74451075+Bozeau@users.noreply.github.com>
2023-02-22 14:41:24 +0100

    Update and rename entry.sh to start_docker.sh
    file responsible for the launch and setup of the mongo db

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t mongodb-container .
 docker run -dp 27017:27017 mongodb-container
-docker exec -t mongodb-container "mongosh < script.js"
+docker exec -t mongodb-container "mongosh < setup_db.js"
